d51260ed62595c0c38762f0cc06f3e4e4cc85798
Anna Khvorostyanova <testprogmath@gmail.com>
2018-07-16 02:13:15 +0300

    Залит первичный вариант фронта

---
+++ Compass/js/main.js
@@ -0,0 +1,147 @@
+$(function() {
+
+	
+	/*----  Плавная прокрутка  ----*/
+
+
+    $("a.scrollto").click(function () {
+        var elementClick = '#'+$(this).attr("href").split("#")[1]
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1200);
+        return false;
+    });
+
+
+    /*----  Модальное окно  ----*/
+
+
+    $('.act-click').click(function (){
+		$('#exampleModal').arcticmodal();
+	});
+
+
+    /*----  Слайдер  ----*/
+
+
+	 $('.comments-slider').slick({
+	 	dots: true,
+	 	infinite:true,
+		arrows: true,
+		slidesToShow: 1,
+		adaptiveHeight: false,
+		edgeFriction: 0.25,
+		responsive: [
+			{
+				breakpoint: 860,
+					settings: {
+						arrows: false,
+					}
+			},
+			{
+				breakpoint: 480,
+					settings: {
+						arrows: false,
+					}
+			}
+			],
+			prevArrow: '<i class="slick-arrow fa reviews__arrows fa-angle-left" aria-hidden="true" style="font-size:40px;"></i>',
+			nextArrow: '<i class="slick-arrow fa reviews__arrows fa-angle-right" aria-hidden="true" style="font-size:40px;"></i>'
+  });
+
+
+
+	 /*----  Проверка данных и отправка формы  ----*/
+
+
+
+ 	 $('[data-submit]').on('click', function(e){
+	    e.preventDefault();
+		$(this).parent('form').submit();
+		})
+	$.validator.addMethod(
+	        "regex",
+	        function(value, element, regexp) {
+	            var re = new RegExp(regexp);
+	            return this.optional(element) || re.test(value);
+	        },
+	        "Please check your input."
+			);
+			function valEl(el){
+		 
+          el.validate({
+        rules:{
+          tel:{
+            required:true,
+            regex: '^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$'
+          },
+          name:{
+            required:true
+          },
+          email:{
+          	required:true,
+            email:true
+          }
+        },
+          messages:{
+            tel:{
+              	required:'Поле обязательно для заполнения',
+              	regex:'Телефон может содержать символы + - ()'
+            },
+            name:{
+              	required:'Поле обязательно для заполнения',
+            },
+            email:{
+            	required:'Поле обязательно для заполнения',	
+            	email:'Неверный формат E-mail'
+            }
+        },            
+        submitHandler: function (form) {
+        	$('#loader').fadeIn();
+	        var $form = $(form);
+	        var $formId = $(form).attr('id');
+	        switch($formId){
+	          case'goToNewPage':
+	            $.ajax({
+	                  type: 'POST',
+	                  url: $form.attr('action'),
+	                  data: $form.serialize(),
+	                })
+	                .always(function (response) {  
+	                    //ссылка на страницу "спасибо" - редирект
+	                    location.href='https://wayup.in/lm/landing-page-marathon/success';
+	                    //отправка целей в Я.Метрику и Google Analytics
+	                    ga('send', 'event', 'masterklass7', 'register');
+			    yaCounter27714603.reachGoal('lm17lead');
+	            });
+	        break;        
+	        case'popupResult':
+            $.ajax({
+                  type: 'POST',
+                  url: $form.attr('action'),
+                  data: $form.serialize(),
+                })
+                .always(function (response) {                    
+                setTimeout(function (){
+                  $('.box-modal').fadeOut();
+                  $.arcticmodal('close');
+                  $('#overlay').fadeIn();
+                  
+                  $form.trigger('reset');
+                  //строки для остлеживания целей в Я.Метрике и Google Analytics
+                },1100);
+                $('#overlay').on('click', function(e) {
+  			        $('#overlay').fadeOut();
+		            });
+                    
+                });
+        	break;          
+       		}       
+		return false; 
+    	}                           
+ 	 	})
+    }                        
+     
+	$('.js-form').each(function() {
+    	valEl($(this)); 
+	});
+});
---
+++ Compass/success.php
@@ -0,0 +1,50 @@
+<?php
+header("Content-Type: text/html; charset=utf-8");
+$email = htmlspecialchars($_POST["email"]);
+$name = htmlspecialchars($_POST["name"]);
+$tel = htmlspecialchars($_POST["tel"]);
+
+$check = is_array($_POST['check']) ? $_POST['check'] : array();
+$check = implode (', ', $check );
+
+$radio = htmlspecialchars($_POST["radio"]);
+
+$refferer = getenv('HTTP_REFERER');
+$date=date("d.m.y"); // число.месяц.год  
+$time=date("H:i"); // часы:минуты:секунды 
+$myemail = "funfot@ya.ru";
+
+$tema = "Тема письма админу";
+$message_to_myemail = "Текст письма:
+<br><br>
+Имя: $name<br>
+E-mail: $email<br>
+Телефон: $tel<br>
+Чек-бокс: $check<br>
+Радио: $radio<br>
+Источник (ссылка): $refferer
+";
+
+mail($myemail, $tema, $message_to_myemail, "From: Sitename <reg@wayup.in> \r\n Reply-To: WAYUP \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
+
+
+$tema = "Тема письма клиенту";
+$message_to_myemail = "
+Текст письма<br>
+Файл: <a href='http://numbers.andrewgavrilov.me/files/ripple.zip' download>Название файла</a>
+
+";
+$myemail = $email;
+mail($myemail, $tema, $message_to_myemail, "From: WAYUP <reg@wayup.in> \r\n Reply-To: WAYUP \r\n"."MIME-Version: 1.0\r\n"."Content-type: text/html; charset=utf-8\r\n" );
+
+
+$f = fopen("leads.xls", "a+");
+fwrite($f," <tr>");    
+fwrite($f," <td>$email</td> <td>$name</td> <td>$tel</td>   <td>$date / $time</td>");   
+fwrite($f," <td>$refferer</td>");    
+fwrite($f," </tr>");  
+fwrite($f,"\n ");    
+fclose($f);
+
+
+?>
